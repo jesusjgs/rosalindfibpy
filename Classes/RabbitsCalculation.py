31288885defeb2772752a2a5330b402461d7b196
jesusjgs <jesusjgs@alu.uma.es>
2018-05-20 22:41:24 +0200

    Readjusting classes error fixing

--- Classes/RabbitsCalculation.py
+++ Classes/RabbitsCalculation.py
@@ -3,17 +3,14 @@
 
 
 class RabbitsCalculation:
-    def __init__(self, ini: int)->None:
-        self.initialization = None
-
     def compute(self, months: int, sons_per_pair: int, earlier_rabbits: int, before_earlier_rabbits) -> int:
-        res = None
         if months > 40 or months < 0 or sons_per_pair > 5 or sons_per_pair < 0:
             raise Exception('Arguments are not the on the valid range')
         else:
             if months > 1:
-                new_value = earlier_rabbits + before_earlier_rabbits * sons_per_pair
-                res = self.compute(self, months - 1, sons_per_pair, new_value, earlier_rabbits)
+                new_months = months - 1
+                updated_early_rabb = earlier_rabbits + before_earlier_rabbits * sons_per_pair
+                res = RabbitsCalculation.compute(self, new_months, sons_per_pair, updated_early_rabb, earlier_rabbits)
             else:
                 res = earlier_rabbits
             return res
--- Classes/RabbitsCalculationTest.py
+++ Classes/RabbitsCalculationTest.py
@@ -15,12 +15,5 @@ from Classes import RabbitsCalculation
 class RabbitsCalculationTest(unittest.TestCase):
 
     def test_should_compute_One_Return_One(self):
-        # Create mock object
-        rabbits: RabbitsCalculation = mock()
-        # Stubing
-        when(rabbits).compute(1, 1, 1, 0).thenReturn(True)
-        # Execute
-        compute_order: RabbitsCalculation = RabbitsCalculation(0)
-        res: int = compute_order.compute(1, 1, 1, 0)
-        # Verify
-        verify(res == 1)
+        res = RabbitsCalculation.compute(1, 1, 1, 0)
+        self.assertEqual(res, 1)
